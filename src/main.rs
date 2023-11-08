f8c5a9ed4a0382d56347fe7e9d03501f88b9f3ce
asuto15 <sluggard0115@gmail.com>
2023-11-08 22:40:56 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,72 @@
+use std::env;
+use std::process::Command;
+use std::collections::HashMap;
+use regex::Regex;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <command>", args[0]);
+        std::process::exit(1);
+    }
+
+    let which_output = Command::new("which")
+        .arg("objdump")
+        .output()?;
+
+    if !which_output.status.success() {
+        eprintln!("objdump not found");
+        std::process::exit(1);
+    }
+
+    let output = Command::new("objdump")
+        .arg("-d")
+        .arg(&args[1])
+        .output()?;
+
+    if !output.status.success() {
+        let error_str = String::from_utf8_lossy(&output.stderr);
+        eprintln!("objdump failed: {}", error_str);
+    }
+
+    let output_str = String::from_utf8_lossy(&output.stdout);
+
+    println!("objdump output:\n{}", output_str);
+    analyze_objdump(&output_str)?;
+
+    Ok(())
+}
+
+fn analyze_objdump(output: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let mut functions = Vec::new();
+    let mut current_function: Option<String> = None;
+    let function_regex = Regex::new(r"([0-9a-f]+) <(.+?)>:")?;
+    let mut instruction_count = 0;
+
+    for line in output.lines() {
+        if let Some(captures) = function_regex.captures(line) {
+            if let Some(func) = current_function.replace(captures[2].to_string()) {
+                functions.push((func, instruction_count));
+            }
+            instruction_count = 0;
+            continue;
+        }
+
+        if line.trim().is_empty() || line.contains(">:") || line.starts_with("Disassembly") {
+            continue;
+        }
+        if line.contains(':') && line.split_whitespace().nth(1).is_some() {
+            instruction_count += 1;
+        }
+    }
+
+    if let Some(func) = current_function {
+        functions.push((func, instruction_count));
+    }
+
+    for (function, count) in functions {
+        println!("{}:\n\t{} instructions", function, count);
+    }
+
+    Ok(())
+}
