273325ffc456c66981c5777f3729eaa90f9a850c
asuto15 <sluggard0115@gmail.com>
2023-11-08 23:35:07 +0900

    feat: c option & analyze_connection

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use regex::Regex;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
-        eprintln!("Usage: {} -i <path-to-binary>", args[0]);
+        eprintln!("Usage: {} -[ic] <path-to-binary>", args[0]);
         std::process::exit(1);
     }
 
-    if args[1] != "-i" {
+    if args[1] != "-i" && args[1] != "-c" {
         eprintln!("Unknown option: {}", args[1]);
         std::process::exit(1);
     }
@@ -38,6 +38,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     if args.contains(&"-i".to_string()) {
         analyze_objdump(&output_str)?;
+    }
+
+    if args.contains(&"-c".to_string()) {
+        analyze_connection(&output_str)?;
     }
 
     Ok(())
@@ -72,6 +76,47 @@ fn analyze_objdump(output: &str) -> Result<(), Box<dyn std::error::Error>> {
 
     for (function, count) in functions {
         println!("{}:\n\t{} instructions", function, count);
+    }
+
+    Ok(())
+}
+
+fn analyze_connection(output: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let mut functions: Vec<(String, Vec<String>)> = Vec::new();
+    let mut current_function: Option<String> = None;
+    let function_regex = Regex::new(r"([0-9a-f]+) <(.+?)>:")?;
+    let child_regex = Regex::new(r"([0-9a-f]+): .+ <(.+?)>")?;
+    let mut children: Vec<String> = Vec::new();
+
+    for line in output.lines() {
+        if let Some(captures) = function_regex.captures(line) {
+            if let Some(func) = current_function.replace(captures[2].to_string()) {
+                functions.push((func, children.clone()));
+                children.clear();
+                continue;
+            }
+
+            if line.trim().is_empty() || line.contains(">:") || line.starts_with("Disassembly") {
+                continue;
+            }
+
+            if let Some(child) = child_regex.captures(line) {
+                children.push(child[2].to_string().clone());
+            }
+
+            continue;
+        }
+    }
+
+    if let Some(func) = current_function {
+        functions.push((func, children));
+    }
+
+    for (function, children) in functions {
+        println!("{}:", function);
+        for child in children {
+            println!("\t{}", child);
+        }
     }
 
     Ok(())
